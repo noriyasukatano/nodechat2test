0c93c44d87cea392370e7f3408543ab962252ff2
sumple_html <nori559259@gmail.com>
2017-01-08 15:24:52 +0900

    change the code

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
+var path = require('path');
 var express = require("express");
 var app = express();
-app.use(express.logger());
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(request, response) {
-  response.send('Hello World webJS!');
+app.use(express.static(path.join(__dirname, 'htdocs')));
+//app.use(express.static('htdocs'));
+
+io.on('connection', function(socket){
+  socket.on('send_message', function(text){
+	io.sockets.emit('receive_message',text);
+  });
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.listen(3000);
+console.log('Server started: http://localhost:3000/');
