9c3d55db6b7bbead6e3f125e7ade2fbd277cda4c
sumple_html <nori559259@gmail.com>
2017-01-08 15:55:02 +0900

    change back

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
-var path = require('path');
+//var path = require('path');
 var express = require("express");
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
 
 app.use(express.logger());
 //app.use(express.static(path.join(__dirname, 'htdocs')));
