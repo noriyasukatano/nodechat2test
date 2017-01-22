469c0b1965818ada2091284119a146a7c4b1bcdb
sumple_html <nori559259@gmail.com>
2017-01-22 17:17:44 +0900

    change

--- web.js
+++ web.js
@@ -4,18 +4,18 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-//app.use(express.logger());
-app.use(express.static(path.join(__dirname, 'htdocs')));
+app.use(express.logger());
+//app.use(express.static(path.join(__dirname, 'htdocs')));
 
 app.get('/', function(request, response) {
   response.send('Hello World webJS!');
 });
 
-io.on('connection', function(socket){
+/* io.on('connection', function(socket){
   socket.on('send_message', function(text){
 	io.sockets.emit('receive_message',text);
   });
-}); 
+}); */ 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
