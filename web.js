def7e3690944525bcbd94ecc50e19ec147c4bbee
sumple_html <nori559259@gmail.com>
2017-01-22 16:15:45 +0900

    change the code

--- web.js
+++ web.js
@@ -1,14 +1,20 @@
 var path = require('path');
 var express = require("express");
 var app = express();
-//var http = require('http').Server(app);
-//var io = require('socket.io')(http);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 //app.use(express.logger());
 app.use(express.static(path.join(__dirname, 'htdocs')));
 
 app.get('/', function(request, response) {
   response.send('Hello World webJS!');
+});
+
+io.on('connection', function(socket){
+  socket.on('send_message', function(text){
+	io.sockets.emit('receive_message',text);
+  });
 });
 
 var port = process.env.PORT || 5000;
