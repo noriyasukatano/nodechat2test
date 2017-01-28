96628a1d732254b196184b3257c9f14a9f3e283f
sumple_html <nori559259@gmail.com>
2017-01-28 12:17:56 +0900

    change fails

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express')
   , http = require('http')
   , app = express()
   ;
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 var port = process.env.PORT || 5000;
 var server = http.createServer(app).listen(port);
 
